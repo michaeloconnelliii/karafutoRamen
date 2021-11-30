4933e48a938e388d5925f602f4acf051e0ab2e5b
Michael O'Connell <michaeloconnelliii@gmail.com>
2021-11-29 23:27:50 -0800

    Got the positioning for small screens and nav started.

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-/* ======== Carousel logic ======== */
+/* ======== Header ======== */
+const openImg = document.getElementById('nav-img-open');
+const closeImg = document.getElementById('nav-img-close');
+const closeBtn = document.querySelector('.nav-close-btn')
+const openBtn = document.querySelector('.nav-open-btn')
+const nav = document.querySelector('nav');
+const carousel = document.querySelector('.carousel');
+
+openImg.addEventListener('click', () => {
+    nav.classList.add('nav-open');
+    carousel.classList.add('is-hidden');
+
+    openBtn.classList.add('is-hidden');
+    closeBtn.classList.remove('is-hidden');
+});
+
+closeImg.addEventListener('click', () => {
+    nav.classList.remove('nav-open');
+    carousel.classList.remove('is-hidden');
+
+    openBtn.classList.remove('is-hidden');
+    closeBtn.classList.add('is-hidden');
+});
+
+/* ======== Carousel ======== */
 const track = document.querySelector('.carousel-track');
 const slides = Array.from(track.children);
 const nextButton = document.querySelector('.carousel-btn-next');
