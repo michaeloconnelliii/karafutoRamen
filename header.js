76a3d3482387abb7237b8d9e8c8111658459056f
Michael O'Connell <michaeloconnelliii@gmail.com>
2021-12-18 23:21:34 -0800

    Prevented header transition on browser resize

--- header.js
+++ header.js
@@ -19,3 +19,22 @@ closeImg.addEventListener('click', () => {
     openBtn.classList.remove('is-hidden');
     closeBtn.classList.add('is-hidden');
 });
+
+/* Stop playing nav transition momentarily on viewport resize. 
+   Source: https://stackoverflow.com/questions/63861504/pure-css-hamburger-menu-shows-up-when-resizing-viewport-before-disappearing */
+   (function () {
+    const classes = document.body.classList;
+    let timer = null;
+    window.addEventListener('resize', function () {
+      if (timer){
+        clearTimeout(timer);
+        timer = null;
+      } else {
+        classes.add('stop-transition');
+      }
+      timer = setTimeout(() => {
+        classes.remove('stop-transition');
+        timer = null;
+      }, 100);
+    });
+  })();
