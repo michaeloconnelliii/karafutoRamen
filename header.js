ddb5c4833586b04be5cb4b611be44b865bee9fa9
Michael O'Connell <michaeloconnelliii@gmail.com>
2021-12-20 14:40:15 -0800

    nav now closes when user expands browser window beyond 725px

--- header.js
+++ header.js
@@ -6,19 +6,22 @@ const openBtn = document.querySelector('.nav-open-btn')
 const nav = document.querySelector('nav');
 const carousel = document.querySelector('.carousel');
 
-openImg.addEventListener('click', () => {
-    nav.classList.add('nav-open');
+function openNav() {
+  nav.classList.add('nav-open');
 
-    openBtn.classList.add('is-hidden');
-    closeBtn.classList.remove('is-hidden');
-});
+  openBtn.classList.add('is-hidden');
+  closeBtn.classList.remove('is-hidden');
+}
 
-closeImg.addEventListener('click', () => {
-    nav.classList.remove('nav-open');
+function closeNav() {
+  nav.classList.remove('nav-open');
 
-    openBtn.classList.remove('is-hidden');
-    closeBtn.classList.add('is-hidden');
-});
+  openBtn.classList.remove('is-hidden');
+  closeBtn.classList.add('is-hidden');
+}
+
+openImg.addEventListener('click', openNav);
+closeImg.addEventListener('click', closeNav);
 
 /* Stop playing nav transition momentarily on viewport resize. 
    Source: https://stackoverflow.com/questions/63861504/pure-css-hamburger-menu-shows-up-when-resizing-viewport-before-disappearing */
@@ -38,3 +41,10 @@ closeImg.addEventListener('click', () => {
       }, 100);
     });
   })();
+
+/* if user resizes browser window from mobile to desktop while nav is open, close the nav */
+window.addEventListener('resize', () => {
+  if(window.innerWidth >= 725) {
+    closeNav();
+  }
+});
